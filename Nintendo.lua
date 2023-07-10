5597a360e812c8bb90eb801cf4656126ee7981a7
Russell Wright Helder <rhelder.git@gmail.com>
2023-07-09 23:15:24 -0700

    Tried out table of tables, started on metatable idea

--- Nintendo.lua
+++ Nintendo.lua
@@ -30,5 +30,51 @@ end
 
 setmetatable(consoles, meta)
 
-print(consoles['NES'])
-print(consoles['Wii'])
+-- nes_response = 'Classic choice!'
+-- trial = {
+--      NES = nes_response
+-- }
+-- 
+-- print(trial.NES)
+-- trial['NES'] = 'Please try again.'
+-- print(trial.NES)
+
+--   tableoftables['NES']['nes'] = 'Classic choice!'
+--   tableoftables['Nintendo Entertainment System']['nes'] = 'Classic choice!'
+--   tableoftables['SNES']['snes'] = 'Super choice!'
+--   tableoftables['Super Nintendo']['snes'] = 'Super choice!'
+--   tableoftables['Super Nintendo']['snes'] = 'Super choice!'
+
+table1 = {
+     nes = 'Classic choice!',
+     snes = 'Super choice!',
+}
+
+table2 = {
+     nes = {'NES', 'Nintendo Entertainment System'},
+     snes = {'SNES', 'Super Nintendo', 'Super Nintendo Entertainment System'},
+}
+
+tableoftables = {}
+for k, a in pairs(table2) do
+     tableoftables[k] = {}
+     for i, v in ipairs(a) do 
+	  tableoftables[k][v] = table1[k]
+     end
+end
+
+-- print(tableoftables['nes']['NES'])
+-- tableoftables['nes'] = 'Please try again.'
+-- print(tableoftables['nes']['Nintendo Entertainment System'])
+-- print(tableoftables['snes']['SNES'])
+-- tableoftables['snes'] = 'Please try again.'
+-- print(tableoftables['snes']['Super Nintendo'])
+-- print(tableoftables['snes']['Super Nintendo Entertainment System'])
+
+metatrial = {
+     nes = {'NES', 'Nintendo Entertainment System'},
+     nes_response = 'Classic choice!'
+     for i, v in ipairs(nes) do
+	  v = nes_response
+     end
+     snes = {'SNES', 'Super Nintendo', 'Super Nintendo Entertainment System'},
