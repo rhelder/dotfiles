1cf3beb4f7498a2a8c99caf4cf978e2e8c46de46
Russell Wright Helder <rhelder.git@gmail.com>
2023-08-12 11:21:55 -0700

    Updated to-do list, renamed `vocab` to `cards`, and varied delay times

--- flashcards.lua
+++ flashcards.lua
@@ -1,34 +1,27 @@
 -- to-do
--- -- Add just view flashcard option
--- -- Add procedure for disambiguating duplicates. Rough idea:
--- -- -- Looping through each line, search that line for search pattern.
--- -- -- Using gn text object, yank search result and set it equal to variable
--- -- -- -- Looping through all of the other lines, yank the results of the search patterns for each of those lines also
--- -- -- -- Each time, compare the yanked text (in register "0) to the variable.
--- -- -- -- If they are, substitute with the same text plus a number
--- -- -- -- Increment the number
--- -- -- Reset number, reset variable, repeat
+-- * Add just view flashcard option
+-- * Add back-to-front review
 
 
 -- title =
 
--- vocab = {}
+-- cards = {}
 
 
--- Loop to automatically find number of vocabulary words
+-- Loop to automatically find number of cards
 
 verba = 0
-for k, v in pairs(vocab) do
+for k, v in pairs(cards) do
      verba = verba + 1
 end
 
 
--- Create an array in which each vocabulary word (i.e., each key in table 'vocab') is paired with a random number
+-- Create an array in which each key in `cards` is paired with a random number
 
 rand = {}
 
 math.randomseed(os.time())
-for k, v in pairs(vocab) do
+for k, v in pairs(cards) do
      local num = math.random(verba)
      -- Make sure there are no duplicates
      while rand[num] ~= nil do
@@ -49,16 +42,16 @@ end
 -- Start reviewing flashcards
 
 print(title)
-sleep(5)
+sleep(10)
 
 review = {}
 reverba = 0
 for i, verbum in ipairs(rand) do
      print(verbum)
-     if io.read() == vocab[verbum] then
+     if io.read() == cards[verbum] then
 	  print('Correct')
-     else print ('Incorrect. Correct answer: ' .. vocab[verbum])
-	  review[verbum] = vocab[verbum]
+     else print ('Incorrect. Correct answer: ' .. cards[verbum])
+	  review[verbum] = cards[verbum]
 	  reverba = reverba + 1
      end
      sleep(5)
@@ -67,9 +60,11 @@ end
 
 -- Start loop for reviewing missed flashcards
 
-for i, verbum in ipairs(rand) do
+print('Review missed cards')
+sleep(10)
 
-     -- Create an array in which each key in table 'review' is paired with a random number (clearing and re-using rand)
+for i, verbum in ipairs(rand) do
+     -- Create an array in which each key in table `review` is paired with a random number (clearing and re-using rand)
      rand = {}
      for k, v in pairs(review) do
 	  local num = math.random(reverba)
@@ -79,15 +74,15 @@ for i, verbum in ipairs(rand) do
 	  rand[num] = k
      end
 
-     -- Start reviewing missed flashcards (clearing and re-using review and reverba)
+     -- Start reviewing missed flashcards (clearing and re-using `review` and `reverba`)
      review = {}
      reverba = 0
      for i, verbum in ipairs(rand) do
 	  print(verbum)
-	  if io.read() == vocab[verbum] then
+	  if io.read() == cards[verbum] then
 	       print('Correct')
-	  else print ('Incorrect. Correct answer: ' .. vocab[verbum])
-	       review[verbum] = vocab[verbum]
+	  else print ('Incorrect. Correct answer: ' .. cards[verbum])
+	       review[verbum] = cards[verbum]
 	       reverba = reverba + 1
 	  end
 	  sleep(5)
