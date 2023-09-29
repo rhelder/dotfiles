a797c258d4c48660ced4463068b18a86022e0d7f
Russell Wright Helder <rhelder.git@gmail.com>
2023-09-29 16:41:27 -0700

    Initial commit of `fzf` configuration file

---
+++ .fzf.zsh
@@ -0,0 +1,18 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
+
+# Use `fd` instead of `find`
+
+export FZF_DEFAULT_COMMAND='fd --hidden --type f --strip-cwd-prefix'
+export FZF_CTRL_T_COMMAND='fd --hidden --strip-cwd-prefix'
