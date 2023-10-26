b6f77ba85ed1629af7eb315ef692c4ad01f5afe1
Russell Wright Helder <rhelder.git@gmail.com>
2023-10-26 10:17:41 -0700

    Delete 'scripts' directory
    To be made into separate repository for in-progress scripts

--- scripts/Favorites/petName.lua
+++
@@ -1,9 +0,0 @@
-print('Please enter your name')
-name = io.read()
-print('Hello, ' .. name)
-do
-     print("Please enter your pet's name")
-     local name = io.read()
-     print(name .. ' is so cute!')
-end
-print('Thank you, ' .. name .. '. Goodbye')
