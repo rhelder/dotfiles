7bc1eb15a97779bcbfae40637ff4614fe4758a24
Russell Wright Helder <rhelder.git@gmail.com>
2024-12-05 20:52:39 -0800

    Initial commit of custom LaTeX Pandoc writer
    Whose purpose is to treat single quotes as outer quotes (\enquote) and
    double quotes as inner quotes (\enquote*)

---
+++ .local/share/pandoc/custom/latex.lua
@@ -0,0 +1,18 @@
+function Writer(doc, opts)
+  local filter = {
+    Quoted = function(element)
+      if element.quotetype == 'SingleQuote' then
+        table.insert(element.content, 1, pandoc.RawInline('latex', '\\enquote{'))
+        table.insert(element.content, pandoc.RawInline('latex', '}'))
+        return element.content
+      elseif element.quotetype == 'DoubleQuote' then
+        table.insert(element.content, 1, pandoc.RawInline('latex', '\\enquote*{'))
+        table.insert(element.content, pandoc.RawInline('latex', '}'))
+        return element.content
+      end
+    end
+  }
+  return pandoc.write(doc:walk(filter), 'latex', opts)
+end
+
+Template = pandoc.template.default 'latex'
