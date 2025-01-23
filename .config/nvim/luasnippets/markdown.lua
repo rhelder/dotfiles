a91b6e682a51ff9cf6ff1a14997914376876d8d3
Russell Wright Helder <rhelder.git@gmail.com>
2025-01-22 17:50:59 -0800

    Add additional Markdown snippets

--- .config/nvim/luasnippets/markdown.lua
+++ .config/nvim/luasnippets/markdown.lua
@@ -1,3 +1,9 @@
 return {}, {
-  s('gc', fmta('[<>]{lang=grc}', {i(1)})),
+  s('gg', fmta('[<>]{lang=grc}', d(1, get_visual))),
+  s('boulesis', t('[βούλησις]{lang=grc}')),
+  s('Boulesis', t('[Βούλησις]{lang=grc}')),
+  s('phronesis', t('[φρόνησις]{lang=grc}')),
+  s('Phronesis', t('[Φρόνησις]{lang=grc}')),
+  s('prohairesis', t('[προαίρεσις]{lang=grc}')),
+  s('Prohairesis', t('[Προαίρεσις]{lang=grc}')),
 }
